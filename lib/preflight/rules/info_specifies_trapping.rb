14ade2666d21059670b7da4aa7d35d46e8591b19
Chris St. John <csj@sharelocalmedia.com>
2021-02-10 14:59:54 -0500

    Adds a new issue instance to Preflight::Rules::InfoHasKeys#check_hash and ::InfoSpecifiesTrapping#check_hash when the optional Info dictionary is not found, rather than raising exception.

--- lib/preflight/rules/info_specifies_trapping.rb
+++ lib/preflight/rules/info_specifies_trapping.rb
@@ -18,15 +18,18 @@ module Preflight
     class InfoSpecifiesTrapping
 
       def check_hash(ohash)
+        array = []
         info = ohash.object(ohash.trailer[:Info])
-
-        if !info.has_key?(:Trapped)
-          [ Issue.new("Info dict does not specify Trapped", self) ]
-        elsif info[:Trapped] != :True && info[:Trapped] != :False
-          [ Issue.new("Trapped value of Info dict must be True or False", self) ]
+        if info.nil?
+          array << Issue.new("Info dict definition is missing", self, :key => key)
         else
-          []
+          if !info.has_key?(:Trapped)
+            array << Issue.new("Info dict does not specify Trapped", self)
+          elsif info[:Trapped] != :True && info[:Trapped] != :False
+            array << Issue.new("Trapped value of Info dict must be True or False", self)
+          end
         end
+        array
       end
     end
   end
